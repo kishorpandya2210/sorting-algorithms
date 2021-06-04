3a1d9bcad52bf889d4991a67bedb695b76da8c13
Kishor Pandya <kishorpandya17@gmail.com>
2021-06-04 17:16:04 +0530

    Made a few correctiona

--- sorting-algorithms/src/App.js
+++ sorting-algorithms/src/App.js
@@ -8,18 +8,24 @@ function App() {
         <div className="array_size">
           <p>Size of array</p>
           <input id="size" type="range" min="20" max="250" step="1" value="80"></input>
+          <br></br>
           <button id="generate">Generate array</button>
         </div>
         <div className="title">
           <h1>Sorting Visualizer</h1>
-          <button>Bubble Sort</button>
-          <button>Selection Sort</button>
-          <button>Insertion Sort</button>
-          <button>Merge Sort</button>
-          <button>Quick Sort</button>
-          <button>Heap Sort</button>
+          <button id="bubble">Bubble Sort</button>
+          <button id="selection">Selection Sort</button>
+          <button id="insertion">Insertion Sort</button>
+          <button id="merge">Merge Sort</button>
+          <button id="quick">Quick Sort</button>
+          <button id="heap">Heap Sort</button>
         </div>
         </nav>
+        <section>
+          <div id="array_container">
+
+          </div>
+        </section>
       </header>
     </div>
   );
