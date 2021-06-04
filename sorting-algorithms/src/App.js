85d0db141aa41e8656e14386c3fd09e907ad72d4
Kishor Pandya <kishorpandya17@gmail.com>
2021-06-04 14:20:59 +0530

    Built basic navbar

--- sorting-algorithms/src/App.js
+++ sorting-algorithms/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello!
-        </a>
+      <header>
+        <nav>
+        <div className="array_size">
+          <p>Size of array</p>
+          <input id="size" type="range" min="20" max="250" step="1" value="80"></input>
+          <button id="generate">Generate array</button>
+        </div>
+        <div className="title">
+          <h1>Sorting Visualizer</h1>
+          <button>Bubble Sort</button>
+          <button>Selection Sort</button>
+          <button>Insertion Sort</button>
+          <button>Merge Sort</button>
+          <button>Quick Sort</button>
+          <button>Heap Sort</button>
+        </div>
+        </nav>
       </header>
     </div>
   );
