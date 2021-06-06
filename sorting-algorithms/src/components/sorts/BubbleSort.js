5cc660ac6c4bbe7c7892f345bc4cf855b9caa504
Kishor Pandya <kishorpandya17@gmail.com>
2021-06-06 14:58:04 +0530

    Finished bubble sort completely

--- sorting-algorithms/src/components/sorts/BubbleSort.js
+++ sorting-algorithms/src/components/sorts/BubbleSort.js
@@ -1,23 +1,39 @@
 import React from 'react'
 
 function BubbleSort({divs}) {
-    
-    for(var i=0;i<divs.length;i++){
-        for(var j=0;j<divs.length-i-1;j++){
-            divs[j].style.backgroundColor="blue";
-            divs[j+1].style.backgroundColor="blue";
-            var a1=parseInt(divs[j].getAttribute("id"));
-            var a2=parseInt(divs[j+1].getAttribute("id"));
-                if(a1>a2){
-                    divs[j].style=" margin:0% 0.1%; background-color:red; width:" + (100/divs.length-(2*0.1)) + "%; height:" + (a2) + "%;";
-                    divs[j+1].style=" margin:0% 0.1%; background-color:red; width:" + (100/divs.length-(2*0.1)) + "%; height:" + (a1) + "%;";
-                    divs[j+1].setAttribute("id",a1.toString());
-                    divs[j].setAttribute("id",a2.toString());
-                }
-            
-            
+
+    var delay_time=10000/(Math.floor(divs.length/10)*100);        //Decrease numerator to increase speed.
+    var c_delay=0;//This is updated ov every div change so that visualization is visible.
+
+    function div_update(cont,height,color){
+        window.setTimeout(function(){
+            cont.style=" margin:0% 0.1%; width:" + (100/divs.length-(2*0.1)) + "%; height:" + height + "%; background-color:" + color + ";";
+        },c_delay+=delay_time);
+    }   
+
+    for(var i=0;i<divs.length-1;i++)
+    {
+        for(var j=0;j<divs.length-i-1;j++)
+        {
+            div_update(divs[j],parseInt(divs[j].getAttribute("id")),"yellow");//Color update
+
+            if(parseInt(divs[j].getAttribute("id"))>parseInt(divs[j+1].getAttribute("id")))
+            {
+                div_update(divs[j],parseInt(divs[j].getAttribute("id")), "red");//Color update
+                div_update(divs[j+1],parseInt(divs[j+1].getAttribute("id")), "red");//Color update
+
+                var temp=divs[j].getAttribute("id");
+                divs[j].setAttribute("id", divs[j+1].getAttribute("id"));
+                divs[j+1].setAttribute("id", temp);
+
+                div_update(divs[j],parseInt(divs[j].getAttribute("id")), "red");//Height update
+                div_update(divs[j+1],parseInt(divs[j+1].getAttribute("id")), "red");//Height update
+            }
+            div_update(divs[j],parseInt(divs[j].getAttribute("id")), "blue");//Color updat
         }
+        div_update(divs[j],parseInt(divs[j].getAttribute("id")), "green");//Color update
     }
+    div_update(divs[0],parseInt(divs[j].getAttribute("id")), "green");//Color update
 
     return (
         <div>
